3514469b9d330d9741800140e9890df11acd6e5d
Abbiesyeh <62372252+Abbiesyeh@users.noreply.github.com>
2021-01-20 13:32:57 +0700

    Update DaoService.java

--- PBO2/src/com/PBO/util/DaoService.java
+++ PBO2/src/com/PBO/util/DaoService.java
@@ -3,9 +3,7 @@ package com.PBO.util;
 import java.sql.SQLException;
 import java.util.List;
 
-/**
- * @author PBO Tan
- */
+
 public interface DaoService<T> {
 
     List<T> fetchAll() throws SQLException, ClassNotFoundException;
