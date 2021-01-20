96a03005e016344c21803ec5b22d7bb44d5c47b0
rhnkbr <62373321+rhnkbr@users.noreply.github.com>
2021-01-20 13:37:56 +0700

    Update MySQLConnection.java

--- PBO2/src/com/PBO/util/MySQLConnection.java
+++ PBO2/src/com/PBO/util/MySQLConnection.java
@@ -4,9 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- * @author PBO Tan
- */
 public class MySQLConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/facultydepartement";
