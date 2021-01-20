663357681e0ce1653cdda023bad802472707e3f1
rhnkbr <62373321+rhnkbr@users.noreply.github.com>
2021-01-20 13:35:58 +0700

    Update DepartmentDaoImpl.java

--- PBO2/src/com/PBO/dao/DepartmentDaoImpl.java
+++ PBO2/src/com/PBO/dao/DepartmentDaoImpl.java
@@ -12,9 +12,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author PBO Tan
- */
 public class DepartmentDaoImpl implements DaoService<Department> {
 
     @Override
